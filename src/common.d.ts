62435201d2a617d1818074e22fd4b7fe5cf2a704
Sina Kamrava <sinakamrava3118@gmail.com>
2023-09-30 18:43:39 +0330

    feat : finish Map compoent and customise that

--- src/common.d.ts
+++ src/common.d.ts
@@ -1,31 +1,28 @@
 type InputType = {
-  icon: React.ReactElement,
- 
-  placeholder: string,
-  error: string,
-  className: string,
-  type: string,
-  tabindex: number,
-  res:[]|undefined
-}
+  icon: React.ReactElement;
 
-
+  placeholder: string;
+  error: string;
+  className: string;
+  type: string;
+  tabindex: number;
+  res: [] | undefined;
+};
 
 type FormValues = {
   name: string;
   lastName: string;
-  email: string,
-  password:string
+  email: string;
+  password: string;
 };
 
-
 type User = {
-  id: number,
-  email:string,
-  password:string,
-  name:string,
-  lastName:string,
-}
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  lastName: string;
+};
 
 type ContentPropsType = {
   children: ReactElement;
@@ -33,4 +30,10 @@ type ContentPropsType = {
 type DataContextValue = {
   user: User;
   setUser: React.Dispatch<React.SetStateAction<User>> | undefined;
+};
+
+type MapType = {
+  position: LatLngExpression;
+  dragable: boolean;
+  onChageLatLng: (value: [lat: number, lng: number]) => void;
 };
--- src/components/common/Map.tsx
+++ src/components/common/Map.tsx
@@ -1,12 +1,33 @@
-import { LatLngExpression ,Icon} from "leaflet";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { LatLngExpression, Icon, LeafletMouseEvent } from "leaflet";
+import {
+  MapContainer,
+  useMap,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvent,
+} from "react-leaflet";
+import { useState } from "react";
 
-const position: LatLngExpression = [51.505, -0.09];
 const customIcon = new Icon({
   iconUrl: "/public/images/marker.png",
   iconSize: [100, 100], // Adjust the size of the icon as needed
 });
-const Map = () => {
+
+const Map = ({
+  position,
+  dragable = true,
+  onChageLatLng = () => {},
+}: Partial<MapType>) => {
+
+
+  const [mapPosition, setMapPosition] = useState<LatLngExpression>(position);
+  const handleChangePosition = (e: LeafletMouseEvent) => {
+    const { lat, lng } = e.latlng;
+    setMapPosition([lat, lng]);
+    onChageLatLng([lat, lng]);
+  };
+
 
   return (
     <section className="flex-1 rounded-lg overflow-hidden bg-slate-200 dark:bg-slate-800 h-full w-full">
@@ -20,15 +41,26 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position} icon={customIcon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
+        <Marker position={mapPosition} icon={customIcon}>
+          <Popup>Your Home</Popup>
         </Marker>
+        <ChangePosition position={mapPosition} />
+        {dragable && <DetectClick onClick={handleChangePosition} />}
       </MapContainer>
-      ,
     </section>
   );
 };
+
+
+function ChangePosition({ position }: { position: LatLngExpression }) {
+  const map = useMap();
+  map.setView(position, 10);
+  return null;
+}
+
+function DetectClick({ onClick }: { onClick: (e: LeafletMouseEvent) => void }) {
+  useMapEvent("click", (e: LeafletMouseEvent) => onClick(e));
+  return null;
+}
 
 export default Map;
